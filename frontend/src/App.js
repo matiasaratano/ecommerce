7aa2a093e622eacdd6424a73a6b33eab7d4b0942
Matias Aratano <matiasaratano@gmail.com>
2022-08-21 19:37:22 -0300

    Readme-images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 
 function App() {
@@ -7,7 +8,26 @@ function App() {
         <a href="/">AratanoCommerce</a> 
       </header>
       <main>
-        List Products
+       <h1>Productos</h1>
+       
+        <div className="products">
+        {data.products.map(product => (
+        <div className="product" key={product._id}>
+            
+            <img src={product.image} alt={product.name} />
+            
+            <div className="product-info">
+              <p>
+                {product.name}
+              </p>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+        </div>))
+       } 
+       </div>
       </main>
     </div>
 
