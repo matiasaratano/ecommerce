07861bdbff99c700f5a8101f032513119d40f82a
Matias Aratano <matiasaratano@gmail.com>
2022-08-21 18:20:24 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">AratanoCommerce</a> 
+      </header>
+      <main>
+        List Products
+      </main>
+    </div>
+
+  );
+}
+
+export default App;
