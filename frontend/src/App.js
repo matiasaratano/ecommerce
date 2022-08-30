177994dd45707d1dd5009148d2e0cd96502cdc66
Matias Aratano <matiasaratano@gmail.com>
2022-08-30 18:16:03 -0300

    signin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomeScreen from "./screens/HomeScreen";
+import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
@@ -11,54 +10,56 @@ import Badge from 'react-bootstrap/Badge';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const { state } = useContext(Store);
   const { cart } = state;
   return (
     <BrowserRouter>
-    <div className='d-flex flex-column site-container'>
-      <header>
-        <Navbar bg="dark" variant="dark">
-          <Container className="justify-content-center">
-            <Navbar.Brand href="/">
-              <img
-                src="/logo.png"
-                width="50"
-                height="34"
-                className="d-inline-block align-top"
-                alt="React Bootstrap logo"
-              />
-            </Navbar.Brand>
-            <LinkContainer to="/">
-              <Navbar.Brand>Templ.</Navbar.Brand>
-            </LinkContainer>      
-            <Nav className="me-auto">
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container className="justify-content-center">
+              <Navbar.Brand href="">
+                <img
+                  src="/logo.png"
+                  width="50"
+                  height="34"
+                  className="d-inline-block align-top"
+                  alt="React Bootstrap logo"
+                />
+              </Navbar.Brand>
+              <LinkContainer to="/">
+                <Navbar.Brand>Templ.</Navbar.Brand>
+              </LinkContainer>
+              <Nav className="me-auto">
                 <Link to="/cart" className="nav-link">
                   Cart
                   {cart.cartItems.length > 0 && (
                     <Badge pill bg="danger">
-                       {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                     </Badge>
                   )}
                 </Link>
               </Nav>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
           </Container>
-        </Navbar>
-      </header>
-      <main>
-      <Container className='mt-3'>
-        <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />}/>
-            <Route path="/cart" element={<CartScreen />} />
-            <Route path="/" element={<HomeScreen />}/>
-        </Routes>
-       </Container>
-      </main>
-      <footer>
-        <div className='text-center'>All rights reserved</div>
-      </footer>
-    </div>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
